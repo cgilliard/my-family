8d6a9076714f84e001c8970d14da5ee1463a9570
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-02-13 11:23:21 -0800

    clean secp256k1

---
+++ scripts/secp256k1zkp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd c/secp256k1-zkp
+if [ ! -f "./configure" ]; then
+	./autogen.sh || exit 1;
+	./configure \
+		--enable-module-schnorrsig \
+		--enable-module-rangeproof \
+		--enable-module-generator \
+		--enable-experimental \
+		--enable-module-aggsig \
+		--enable-module-commitment || exit 1;
+fi
+make || exit 1;
+cp .libs/libsecp256k1.a ../.obj
+cd ../..
